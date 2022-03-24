6ca99e976cfbb073f586fc4b36bed199127869dc
jadenlohh <jadenloh24@gmail.com>
2022-03-24 23:26:37 +0800

    Revamped layout and design

--- index.js
+++ index.js
@@ -1,133 +1,72 @@
-// converts 24h to 12h time
-function tConvert(time) {
-    // Check correct time format and split into components
-    time = time.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
-  
-    if (time.length > 1) { // if time format correct
-      time = time.slice(1);  // remove full string match value
-      time[5] =+ time[0] < 12 ? 'AM' : 'PM'; // set AM/PM
-      time[0] =+ time[0] % 12 || 12; // adjust hours
-    }
-    return time.join (''); // return adjusted time or original string
-}
+$.get('https://api.data.gov.sg/v1/environment/24-hour-weather-forecast', (body) => {
+    const data = body.items[0].general;
+
+    $('.weather-24h').html(data.forecast);
+
+    $('.temperature-24h-low').html(data.temperature.low);
+    $('.temperature-24h-high').html(data.temperature.high);
+
+    $('.humidity-24h-low').html(data.relative_humidity.low);
+    $('.humidity-24h-high').html(data.relative_humidity.high);
 
 
-// 24h forecasts
-$.get('https://api.data.gov.sg/v1/environment/24-hour-weather-forecast', function(body, status){
-    var data = body.items[0].general;
-
-    $('#24hForecast').html(data.forecast);
-    $('#24hTemperature').html(data.temperature.low + ' - ' + data.temperature.high + '&deg;C');
-    $('#humidity').html(data.relative_humidity.low + ' - ' + data.relative_humidity.high + '%');
-    $('#windSpeed').html(data.wind.direction + ' ' + data.wind.speed.low + ' - ' + data.wind.speed.high + 'km/h');
-});
-  
-
-// 6h forecasts
-var north_forecast, central_forecast, south_forecast, east_forecast, west_forecast;
-$.ajax({
-    async: false,
-    url: 'https://api.data.gov.sg/v1/environment/24-hour-weather-forecast',
-    type: "GET",
-    success: function(data){ 
-        var region = data.items[0].periods[0].regions;
-
-        // gets string from json and removing '+08:00'
-        var start_time = data.items[0].periods[0].time.start.replace('+08:00', '');
-        var end_time = data.items[0].periods[0].time.end.replace('+08:00', '');
-        var updated_timestamp = data.items[0].timestamp.replace('+08:00', '');
-
-        // split date and time from string
-        start_array = start_time.split('T');
-        end_array = end_time.split('T');
-        updatedTime_array = updated_timestamp.split('T');
-
-        $('#timestamp').html(`From ${start_array[0]} ${tConvert(start_array[1])} to ${end_array[0]} ${tConvert(end_array[1])}`);
-        $('#updated_timestamp').html(`2) Last updated on ${updatedTime_array[0]} ${tConvert(updatedTime_array[1])}`);
-
-        north_forecast = region.north;
-        central_forecast = region.central;
-        south_forecast = region.south;
-        east_forecast = region.east;
-        west_forecast = region.west;
-    }
+    $('.wind-24h-low').html(data.wind.speed.low);
+    $('.wind-24h-high').html(data.wind.speed.high);
 });
 
 
-// configure map
-var mymap = L.map('mapid').setView([1.3521, 103.8198], 11.2);
+$.get('https://api.data.gov.sg/v1/environment/4-day-weather-forecast', (body) => {
+    const data = body.items[0].forecasts;
+    $('.temperature-24h-low').html(data[0].temperature.low);
+    $('.temperature-24h-high').html(data[0].temperature.high);
 
-// set map markers cordinate
-var north_marker = L.marker([1.43, 103.82]).addTo(mymap);
-var east_marker = L.marker([1.345, 103.946]).addTo(mymap);
-var south_marker = L.marker([1.28, 103.815]).addTo(mymap);
-var west_marker = L.marker([1.33, 103.7]).addTo(mymap);
-var central_marker = L.marker([1.35, 103.815]).addTo(mymap);
-
-// set map marker cordinate description
-north_marker.bindPopup(`<b>North</b><br>Forecast: ${north_forecast}`).openPopup();
-east_marker.bindPopup(`<b>East</b><br>Forecast: ${east_forecast}`).openPopup();
-south_marker.bindPopup(`<b>South</b><br>Forecast: ${south_forecast}`).openPopup();
-west_marker.bindPopup(`<b>West</b><br>Forecast: ${west_forecast}`).openPopup();
-central_marker.bindPopup(`<b>Central</b><br>Forecast: ${central_forecast}`).openPopup();
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 11.2,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiY29sZGZyb3N0IiwiYSI6ImNrc245cGs5aTNmN2QydXF0eWU4Y25odGQifQ.YHD8A7B7WW10JJAh4B50aw'
-}).addTo(mymap);
+    $('.humidity-24h-low').html(data[0].relative_humidity.low);
+    $('.humidity-24h-high').html(data[0].relative_humidity.high);
 
 
-// 4 day forecasts
-$.get('https://api.data.gov.sg/v1/environment/4-day-weather-forecast', function(body, status){
-    var data = body.items[0].forecasts;
-    for (i in data) {
-        var date = new Date(data[i].date);
-        var day = date.getDay();
+    $('.wind-24h-low').html(data[0].wind.speed.low);
+    $('.wind-24h-high').html(data[0].wind.speed.high);
 
-        if (day == 0) {day = 'Sunday'}
-        else if (day == 1) {day = 'Monday'}
-        else if (day == 2) {day = 'Tuesday'}
-        else if (day == 3) {day = 'Wednesday'}
-        else if (day == 4) {day = 'Thursday'}
-        else if (day == 5) {day = 'Friday'}
-        else if (day == 6) {day = 'Saturday'}
-
-        $(`#day${i}`).html(day);
-        $(`#day${i}_forecast`).html(data[i].forecast);
-        $(`#day${i}_humidity`).html(`${data[i].relative_humidity.low} - ${data[i].relative_humidity.high}%`);
-        $(`#day${i}_temperature`).html(`${data[i].temperature.low} - ${data[i].temperature.high}&deg;C`);
-        $(`#day${i}_windSpeed`).html(`${data[i].wind.direction} ${data[i].wind.speed.low} - ${data[i].wind.speed.high}km/h`);
-    }
+    $('.weather-day1').html(data[0].forecast);
 });
 
 
+function showMoreReadings() {
+    $('.arrow-down').css('display', 'none');
+    $('.weather-container').css('animation', 'slideDown 800ms forwards');
 
-function showForecast() {
-    $.get('https://api.data.gov.sg/v1/environment/2-hour-weather-forecast', function(body, status){
-        var selected_area = document.getElementById('chooseLocation').value;
+    setTimeout(() => {
+        $('.more-readings').css('display', 'block');
+        $('.weather-container').css('overflow-y', 'scroll');
+    }, 800);
 
-        for (i in body.items[0].forecasts) {
-            if (body.items[0].forecasts[i].area == selected_area) {
-                var start_time = body.items[0].valid_period.start.replace('+08:00', '');
-                var end_time = body.items[0].valid_period.end.replace('+08:00', '');
-                var updated_timestamp = body.items[0].timestamp.replace('+08:00', '');
+    $.get('https://api.data.gov.sg/v1/environment/4-day-weather-forecast', (body) => {
+        const data = body.items[0].forecasts;
+        var num = 1;
+        
+        for (i in data) {
+            var date = new Date(data[i].date);
+            var day = date.getDay();
+            const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-                // split date and time from string
-                start_array = start_time.split('T');
-                end_array = end_time.split('T');
-                updatedTime_array = updated_timestamp.split('T');
 
-                document.getElementById('terms').style.display = 'block';
-                $('#timestamp-2h').html(`From ${start_array[0]} ${tConvert(start_array[1])} to ${end_array[0]} ${tConvert(end_array[1])}`);
-                $('#updated_timestamp-2h').html(`2) Last updated on ${updatedTime_array[0]} ${tConvert(updatedTime_array[1])}`);
-
-                $('#location').html(body.items[0].forecasts[i].area);
-                $('#location-forecast').html(`The weather is ${body.items[0].forecasts[i].forecast}`);
-            } 
-        }
+            $(`.day${num}`).html(days[day]);
+            $(`.weather-d${num}`).html(data[i].forecast);
+            $(`.humidity-d${num}`).html(`${data[i].relative_humidity.low} - ${data[i].relative_humidity.high}`);
+            $(`.temperature-d${num}`).html(`${data[i].temperature.low} - ${data[i].temperature.high}`);
+            
+            num ++;
+        };
     });
-}
+};
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiY29sZGZyb3N0IiwiYSI6ImNsMTNyOW85bjAwYTgzb3A4eWNvcjJ2N2gifQ.nQs70edcWpUaUMWuXEBg2w';
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/outdoors-v11',
+    center: [103.82, 1.35],
+    zoom: 10.5
+});
+// new mapboxgl.Marker().setLngLat([103.839, 1.375]).addTo(map);
